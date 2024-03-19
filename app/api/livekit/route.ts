be6df18e3957921f1a269cc0e30f9ced6c231685
MinilikBelachew <belachewr35@gmail.com>
2024-03-20 00:18:44 +0300

    first commit

---
+++ app/api/livekit/route.ts
@@ -0,0 +1,63 @@
+import { AccessToken } from "livekit-server-sdk";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const room = req.query.room as string;
+  const username = req.query.username as string;
+  if (req.method !== "GET") {
+    return res.status(400).json({ error: "Invalid method" });
+  } else if (!room) {
+    return res.status(400).json({ error: 'Missing "room" query parameter' });
+  } else if (!username) {
+    return res.status(400).json({ error: 'Missing "username" query parameter' });
+  }
+
+  const apiKey = process.env.LIVEKIT_API_KEY;
+  const apiSecret = process.env.LIVEKIT_API_SECRET;
+  const wsUrl = process.env.NEXT_PUBLIC_LIVEKIT_URL;
+
+  if (!apiKey || !apiSecret || !wsUrl) {
+    return res.status(500).json({ error: "Server misconfigured" });
+  }
+
+  const at = new AccessToken(apiKey, apiSecret, { identity: username });
+
+  at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
+  res.status(200).json({ token: await at.toJwt() });
+}
+
+// import { AccessToken } from "livekit-server-sdk";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function GET(req: NextRequest) {
+//   const room = req.nextUrl.searchParams.get("room");
+//   const username = req.nextUrl.searchParams.get("username");
+//   if (!room) {
+//     return NextResponse.json(
+//       { error: 'Missing "room" query parameter' },
+//       { status: 400 }
+//     );
+//   } else if (!username) {
+//     return NextResponse.json(
+//       { error: 'Missing "username" query parameter' },
+//       { status: 400 }
+//     );
+//   }
+
+//   const apiKey = process.env.LIVEKIT_API_KEY;
+//   const apiSecret = process.env.LIVEKIT_API_SECRET;
+//   const wsUrl = process.env.NEXT_PUBLIC_LIVEKIT_URL;
+
+//   if (!apiKey || !apiSecret || !wsUrl) {
+//     return NextResponse.json(
+//       { error: "Server misconfigured" },
+//       { status: 500 }
+//     );
+//   }
+
+//   const at = new AccessToken(apiKey, apiSecret, { identity: username });
+
+//   at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
+
+//   return NextResponse.json({ token: at.toJwt() });
+// }
